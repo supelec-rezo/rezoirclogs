6c74e5d53d922f2b78030be959671babc99eb88c
Georges Dubus <georges.dubus@compiletoi.net>
2012-04-05 22:55:53 +0200

    Some deployment automation.

---
+++ fabfile.py
@@ -0,0 +1,22 @@
+from fabric.api import *
+from fabric.contrib.files import *
+
+env.hosts = ['anakin.rez-gif.supelec.fr']
+
+
+def reload():
+    sudo('/etc/init.d/apache2 reload')
+
+
+def deploy():
+    with cd('/var/lib/rezoirclogs'):
+        # put crashes for me
+        # put('production.ini', '.')
+        run('wget https://raw.github.com/madjar/rezoirclogs/master/production.ini -O production.ini')
+        if not exists('venv'):
+            run('virtualenv venv')
+        run('venv/bin/pip install -e git://github.com/madjar/rezoirclogs.git#egg=rezoirclogs')
+        with cd('venv/'):
+            run('wget https://raw.github.com/madjar/rezoirclogs/master/pyramid.wsgi -O pyramid.wsgi')
+        # put('pyramid.wsgi', 'venv/')
+    reload()
